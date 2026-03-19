f72d56d3b13398b50090efb83ba6ef29f316c3e9
cvlrnz <100946760+cvlrnz@users.noreply.github.com>
2026-03-19 11:03:44 +0800

    add src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Scout from './Scout.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Scout />
+  </React.StrictMode>
+)
